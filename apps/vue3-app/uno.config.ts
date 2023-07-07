5c801fed5319e488b668d8fc510bbe6f47374a09
君惜 <xinlei3166@126.com>
2023-07-07 18:28:17 +0800

    feat: unocss

--- apps/vue3-app/uno.config.ts
+++ apps/vue3-app/uno.config.ts
@@ -13,22 +13,21 @@ export default defineConfig({
     presetRemToPx()
   ],
   // opacity: active, disabled
-  rules: [[/^opacity-brand-(\w+)$/, ([, d]) => ({ opacity: `var(--van-${d}-opacity)` })]],
+  rules: [[/^opacity-(active|disabled)$/, ([, d]) => ({ opacity: `var(--van-${d}-opacity)` })]],
   theme: {
     colors: {
-      brand: {
-        primary: 'var(--van-primary-color)', // class="bg-brand-primary"
-        success: 'var(--van-success-color)',
-        danger: 'var(--van-danger-color)',
-        warning: 'var(--van-warning-color)',
-        text: 'var(--van-text-color)',
-        text2: 'var(--van-text-color-2)',
-        text3: 'var(--van-text-color-3)',
-        active: 'var(--van-active-color)',
-        background: 'var(--van-background)',
-        background2: 'var(--van-background2)',
-        border: 'var(--van-border-color)'
-      }
+      primary: 'var(--van-primary-color)', // class="bg-primary"
+      success: 'var(--van-success-color)',
+      error: 'var(--van-danger-color)',
+      danger: 'var(--van-danger-color)',
+      warning: 'var(--van-warning-color)',
+      text: 'var(--van-text-color)',
+      text2: 'var(--van-text-color-2)',
+      text3: 'var(--van-text-color-3)',
+      active: 'var(--van-active-color)',
+      background: 'var(--van-background)',
+      background2: 'var(--van-background2)',
+      border: 'var(--van-border-color)'
     }
   }
 })
