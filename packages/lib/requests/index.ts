b9b846277d72aed76a19b319aa284a6f57fd175b
君惜 <xinlei3166@126.com>
2026-03-31 14:59:02 +0800

    feat: 添加令牌刷新功能和请求配置更新

--- packages/lib/requests/index.ts
+++ packages/lib/requests/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import { showToast, showLoadingToast, closeToast } from 'vant'
 import type { Config, InternalConfig, RequestsConfig, Method } from '@packages/types'
-import { httpMsg } from '@packages/types/enums'
+import { ContentTypeEnum } from '@packages/types/enums'
 import { getToken, writeFile, writeBase64File } from '@packages/utils'
+// import { useTokenRefresh } from './useTokenRefresh'
 
 let toast: any
 function startLoading(showLoading = false) {
@@ -20,21 +21,23 @@ function endLoading(showLoading = false) {
 
 const createRequests = (requestsConfig: RequestsConfig = {}) => {
   const baseURL = requestsConfig.baseURL || import.meta.env.VITE_API_URL
-  const AuthorizationKey = requestsConfig.AuthorizationKey || 'Access-Token'
+  const authorizationKey = requestsConfig.authorizationKey || 'Authorization'
   const errorCodes = requestsConfig.errorCodes || [500, 400]
   const codeKey = requestsConfig.codeKey || 'code'
   const messageKey = requestsConfig.messageKey || 'message'
   const successCode = requestsConfig.successCode || 0
   const errorHandler = requestsConfig.errorHandler
+  // const noRefreshToken = requestsConfig.noRefreshToken
+  // const refreshTokenApi = requestsConfig.refreshTokenApi
 
   const service = axios.create({
     baseURL,
     // timeout: 60 * 1000 * 5,
     withCredentials: true, // 允许携带cookie
     headers: {
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+      // 'Content-Type': ContentTypeEnum.FormURLEncoded,
       // Authorization: 'token',
-      'Content-Type': 'application/json'
+      'Content-Type': ContentTypeEnum.Json
     },
     requestOptions: {
       showLoading: false,
@@ -44,6 +47,13 @@ const createRequests = (requestsConfig: RequestsConfig = {}) => {
     }
   } as Config)
 
+  // const { handleRefreshed } = useTokenRefresh({
+  //   authorizationKey,
+  //   successCode,
+  //   errorHandler,
+  //   refreshTokenApi
+  // })
+
   // request 拦截器
   service.interceptors.request.use(
     async (config: InternalConfig) => {
@@ -51,7 +61,7 @@ const createRequests = (requestsConfig: RequestsConfig = {}) => {
       config.headers = config.headers || {}
       const token = getToken()
       if (token) {
-        config.headers[AuthorizationKey] = token
+        config.headers[authorizationKey] = token
       }
       startLoading(config?.requestOptions?.showLoading)
       return config
@@ -75,6 +85,10 @@ const createRequests = (requestsConfig: RequestsConfig = {}) => {
         errorHandler?.(msg)
         return response.data
       }
+      // token过期，需要续期
+      // if (code && code === 20011 && !noRefreshToken) {
+      //   return handleRefreshed(service, response.config)
+      // }
       if (code && code !== successCode) {
         closeToast()
         showToast(msg)
