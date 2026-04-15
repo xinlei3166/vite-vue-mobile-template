dc3dcc3d4b022b2d99b84437323515831420565b
君惜 <xinlei3166@126.com>
2026-04-16 02:18:15 +0800

    fix: 将 a-card 组件替换为 div 以修复模板结构

--- apps/vue3-app/src/components/HelloWorld.vue
+++ apps/vue3-app/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-card class="card" style="padding-left: 64px">
+  <div class="card" style="padding-left: 64px">
     <div>
       <img alt="Vue logo" src="../assets/logo.png" />
     </div>
@@ -37,7 +37,7 @@
       <code>components/HelloWorld.vue</code>
       to test hot module replacement.
     </p>
-  </a-card>
+  </div>
 </template>
 
 <script lang="ts">
