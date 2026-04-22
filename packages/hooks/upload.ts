40f8f4c5ff761189f9c0a02b7718a5c2248af6f7
君惜 <xinlei3166@126.com>
2026-04-22 19:34:37 +0800

    fix: 移除 onChange 方法，简化上传逻辑

--- packages/hooks/upload.ts
+++ packages/hooks/upload.ts
@@ -132,17 +132,6 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
   }
   const requestMethod = createRequestMethod()
 
-  const onChange = ({ fileList: newFileList }: any) => {
-    const list = newFileList.map((f: any) => {
-      if (f.response) {
-        f.url = f.response.url
-      }
-      return f
-    })
-    fileList.value = list
-    console.log('fileList', list)
-  }
-
   return {
     fileList,
     uploading,
@@ -151,7 +140,6 @@ export function useUpload({ maxCount, maxSize, accept, upload = true }: UploadCo
     fakeRequestMethod,
     onUpload: requestMethod,
     requestMethod,
-    createRequestMethod,
-    onChange
+    createRequestMethod
   }
 }
